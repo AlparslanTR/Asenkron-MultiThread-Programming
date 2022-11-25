53906a019fd1bd860d177e983bcdbb269fa1e4fb
Alparslan AKBAŞ <bybluestht@gmail.com>
2022-11-25 17:32:59 +0300

    İlk Örnek Uygulandı.

---
+++ AsenkronWebAPI/Controllers/OrnekController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsenkronWebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class OrnekController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetContentAsync()
+        {
+            var myText = new HttpClient().GetStringAsync("https://www.google.com"); // Google Datasını Al.
+            var data = await myText;
+            return Ok(data);
+        }
+    }
+}
