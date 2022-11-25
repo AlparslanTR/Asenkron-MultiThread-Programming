667c7b0a47f32b7b1b17d9b9c5b46abffe3452f8
Alparslan AKBAŞ <bybluestht@gmail.com>
2022-11-25 21:17:52 +0300

    WhenAll Task Methodu Yapıldı.

---
+++ TaskConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Read(Task<string> data)
+    {
+        Console.WriteLine("Google Data Uzunluğu: " + data.Result.Length);
+    }
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var myTask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith(Read);
+        Console.WriteLine("Arada Yapılacak İşler");
+        await myTask;
+    }
+}
---
+++ TaskConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Net.WebRequestMethods;
+
+public class Content
+{
+    public string Site { get; set; }
+    public int Lenght { get; set; }
+}
+public class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Main Thread: " + Thread.CurrentThread.ManagedThreadId);
+
+        List<string> urlList = new List<string>()
+        {
+            "https://www.google.com",
+            "https://www.amazon.com",
+            "https://www.microsoft.com"
+        };
+        List<Task<Content>> taskList = new List<Task<Content>>();
+        urlList.ToList().ForEach(x =>
+        {
+            taskList.Add(GetContentAsync(x));
+        });
+        var contents = await Task.WhenAll(taskList.ToArray());
+        contents.ToList().ForEach(x =>
+        {
+            Console.WriteLine($"{x.Site} boyut:{x.Lenght}");
+        });
+    }
+
+    public static async Task <Content> GetContentAsync(string url)
+    {
+        Content content = new Content();
+        var data =await new HttpClient().GetStringAsync(url);
+        content.Site = url;
+        content.Lenght = data.Length;
+        Console.WriteLine("GetContentAsync Thread: " + Thread.CurrentThread.ManagedThreadId);
+        return content;
+    }
+}
