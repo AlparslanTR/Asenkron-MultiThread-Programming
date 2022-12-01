e34ec07c28928d80dcb00a41551446c051643578
Alparslan AKBAŞ <bybluestht@gmail.com>
2022-12-01 16:57:05 +0300

    ForEach ile drawing kütüphanesi kullanarak resimleri thumbnaile çevirme işlemi yapıldı.

---
+++ ForEachPararelApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string pathPictures = "C:\\Users\\byblu\\Desktop\\Pictures";
+        var files= Directory.GetFiles(pathPictures);
+        Parallel.ForEach(files, file =>
+        {
+            Console.WriteLine("Çalışan Thread No: "+Thread.CurrentThread.ManagedThreadId);
+            Image img = new Bitmap(file);
+            var thumbnail = img.GetThumbnailImage(50, 50, () => false, IntPtr.Zero);
+            thumbnail.Save(Path.Combine(pathPictures,"thumbnail",Path.GetFileName(file)));
+        });
+        Console.WriteLine("İşlem Bitti");
+    }
+}
