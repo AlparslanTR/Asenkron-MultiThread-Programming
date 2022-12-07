660b0e097a593bbeedddb336b14eb59124c9b461
Alparslan AKBAŞ <bybluestht@gmail.com>
2022-12-07 16:01:04 +0300

    PLINQ sorgusuyla örnek yapıldı.

---
+++ PLINQApp/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var array = Enumerable.Range(1, 100).ToList();
+        var newArray= array.AsParallel().Where(x=>x %2== 0 && x>20); // 20 ' den 100 e kadar 2 ye bölümünden 0 kalanları getir.
+        newArray.ToList().ForEach(x=>
+        Console.WriteLine(x));
+    }
+}
